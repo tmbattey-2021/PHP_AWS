23241dd29dc74f93c120fd88aea9845de842cd02
tmbattey-2021 <55719861+tmbattey-2021@users.noreply.github.com>
2022-03-07 17:12:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mell Battey</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Autos Database</h1>
+<p>
+<a href="autos.php">Enter Autos</a>
+</p>
+</div>
+</body>
+</html>
