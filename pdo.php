fc487e66a48f07e3f8e2f130fd37637cdcfeb558
tmbattey-2021 <55719861+tmbattey-2021@users.noreply.github.com>
2023-01-29 18:28:38 -0500

    Add files via upload

--- pdo.php
+++ pdo.php
@@ -1,7 +1,11 @@
 <?php
+//aws mysql database
+$pdo = new PDO('mysql:host=ls-25e22aea68953870fcc0d34c723d0d60e8a81892.cvwgk0rnptsm.us-east-1.rds.amazonaws.com;port=3306;dbname=dbmaster', 
+  'dbmasteruser', 'Darwin01$');
 
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=dbmaster', 
-   'tmbattey', 'darwin');
+//local sql
+//$pdo = new PDO('mysql:host=localhost;port=3306;dbname=dbmaster', 
+//   'tmbattey', 'darwin');
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
